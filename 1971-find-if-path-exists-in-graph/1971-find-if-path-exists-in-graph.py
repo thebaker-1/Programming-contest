616befd6f790018602132d32aa7efdc467802dc8
thebaker-1 <143897248+thebaker-1@users.noreply.github.com>
2024-07-02 11:02:10 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,23 +1,28 @@
 class Solution:
     def validPath(self, n, edges,source, destination):
+        if source == destination:
+            return True
         graph = defaultdict(list)
         for start,end in edges:
             graph[start].append(end)
             graph[end].append(start)
-        # bfs traversal using queue
-        queue = deque()
-        queue.append(source)
-        seen = set()
-        seen.add(source)
-        while queue:
-            node = queue.popleft()
+        # bfs traversal using recursion
+        seen = []
+        def bfs(node,seen):
             if node == destination:
                 return True
             for nei in graph[node]:
                 if nei not in seen:
-                    seen.add(nei)
-                    queue.append(nei)
-        return False
+                    seen.append(nei)
+                    if nei == destination:
+                        return True
+                    res = bfs(nei,seen)
+                    if res:
+                        return True
+            return False
+
+
+        return bfs(source,seen)
 
 
 
