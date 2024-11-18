80c1b85d78b9909b069bd1be632ea174a31246b6
Estifanos Samson <143897248+thebaker-1@users.noreply.github.com>
2024-11-18 20:09:46 +0300

    Time: 57 ms (25.76%) | Memory: 16.7 MB (25.96%) - LeetSync

--- 3463-alternating-groups-i/alternating-groups-i.py
+++ 3463-alternating-groups-i/alternating-groups-i.py
@@ -4,10 +4,11 @@ class Solution:
         for i in range(len(colors)-2):
             if colors[i] != colors[i+1] and colors[i] == colors[i+2]:
                 count +=1
-        if colors[0] != colors[-1] and colors[1] == colors[-1]:
-            count+=1
-        if colors[0] != colors[-1] and colors[0] == colors[-2]:
-            count+=1
+        if colors[0] != colors[-1]:
+            if colors[1] == colors[-1]:
+                count+=1
+            if colors[0] == colors[-2]:
+                count+=1
         return count
 
 
