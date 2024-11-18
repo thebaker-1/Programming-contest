53ec04695e74bbff29712fe4def72f78a86ea57b
Estifanos Samson <143897248+thebaker-1@users.noreply.github.com>
2024-11-18 19:44:47 +0300

    Time: 121 ms (54.67%) | Memory: 36.9 MB (6.08%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,26 +1,21 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        max_sum = float("-inf")
-        cur_sum = 0
-        max_unique_value = 0
-        i = 0
-        unique = defaultdict(int)
-        for j in range(len(nums)):
-            cur_sum += nums[j]
-            unique[nums[j]]+=1
-            max_unique_value = max(max_unique_value,unique[nums[j]])
-            if j-i+1 >=k:
-                if max_unique_value ==1:
-                    max_sum = max(max_sum,cur_sum)
-                cur_sum-=nums[i]
-                if unique[nums[i]] == 1:
-                    unique.pop(nums[i])
-                else:
-                    unique[nums[i]]-=1
-                    max_unique_value = max(unique.values())
-                i+=1
-        
-        return max_sum if max_sum != float("-inf") else 0
+        ans = 0
+        current_sum = 0
+        begin = 0
+        end = 0
+        num_to_index = {}
 
-            
-        
+        while end < len(nums):
+            curr_num = nums[end]
+            last_occurrence = num_to_index.get(curr_num, -1)
+            # if current window already has number or if window is too big, adjust window
+            while begin <= last_occurrence or end - begin + 1 > k:
+                current_sum -= nums[begin]
+                begin += 1
+            num_to_index[curr_num] = end
+            current_sum += nums[end]
+            if end - begin + 1 == k:
+                ans = max(ans, current_sum)
+            end += 1
+        return ans
