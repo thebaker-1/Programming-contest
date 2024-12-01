6c5255ab7812871f5fd743a06e982f560ca3314e
Estifanos Samson <143897248+thebaker-1@users.noreply.github.com>
2024-12-01 22:03:19 +0300

    Time: 1 ms (63.83%) | Memory: 17.4 MB (5.25%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -1,8 +1,10 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
+        sets = set(arr)
         for i in range(len(arr)):
-            for j in range(len(arr)):
-                if i!=j and arr[i]*2 == arr[j]:
+                if arr[i]*2 in sets and arr[i]:
                     return True
+        dic = Counter(arr)
+        if dic[0]>1:return True
         return False
         
