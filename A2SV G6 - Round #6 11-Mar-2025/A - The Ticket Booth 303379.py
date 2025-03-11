083bb113bb41deb7b7ce9b7f6dbd915d8b6c613e
Estifanos Samson <143897248+thebaker-1@users.noreply.github.com>
2025-03-11 23:28:11 +0300

    Created 'A - The Ticket Booth' by A2SV Hub

---
+++ A2SV G6 - Round #6 11-Mar-2025/A - The Ticket Booth 303379.py
@@ -0,0 +1,31 @@
+# Problem: A - The Ticket Booth - https://codeforces.com/gym/594077/problem/A
+
+import sys
+input = sys.stdin.readline
+
+def solve():
+    # n = int(input())
+    # Read two integers separated by space
+    n, m = map(int, input().split())
+    if m % n :
+        print(m // n + 1)
+    else:
+        print(m // n)
+    # Read a list of integers separated by space
+    # nums = list(map(int, input().split()))
+    # Read a list of integers separated by a comma
+    # nums = list(map(int, input().split(',')))
+    # Read a list of words separated by space
+    # words = input().split()
+    # Read a list of words separated by a comma
+    # words = input().split(',')
+    
+
+def main():
+    # t = int(input())
+    t = 1
+    for _ in range(t):
+        solve()
+
+if __name__ == "__main__":
+    main()
