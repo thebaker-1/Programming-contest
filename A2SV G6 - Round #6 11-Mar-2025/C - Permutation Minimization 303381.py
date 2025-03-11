43d11a4fc5ced22a00388a9bd9516ee2a299d383
Estifanos Samson <143897248+thebaker-1@users.noreply.github.com>
2025-03-11 23:28:18 +0300

    Created 'C - Permutation Minimization' by A2SV Hub

---
+++ A2SV G6 - Round #6 11-Mar-2025/C - Permutation Minimization 303381.py
@@ -0,0 +1,26 @@
+# Problem: C - Permutation Minimization - https://codeforces.com/gym/594077/problem/C
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def solve():
+    n = int(input())
+    nums = list(map(int, input().split()))
+    q = deque()
+    for i in range(len(nums)):
+        if not q or nums[i] <= q[0]:
+            q.appendleft(nums[i])
+        else:
+            q.append(nums[i])
+    print(*q)
+    
+
+def main():
+    t = int(input())
+    #t = 1
+    for _ in range(t):
+        solve()
+
+if __name__ == "__main__":
+    main()
