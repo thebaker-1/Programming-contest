1efde8267ddea725c92bb446df565141dfc3583a
Estifanos Samson <143897248+thebaker-1@users.noreply.github.com>
2025-03-11 23:29:06 +0300

    Created 'A - Kibr and His Musics' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 11-Mar-2025/A - Kibr and His Musics 303384.py
@@ -0,0 +1,29 @@
+# Problem: A - Kibr and His Musics - https://codeforces.com/gym/589822/problem/A
+
+from bisect import bisect_left
+import sys
+input = sys.stdin.readline
+
+def solve():
+    n, m = map(int, input().split())
+    sums = 0
+    res = []
+    for _ in range(n):
+        c,t  = map(int, input().split())
+        sums += c*t
+        res.append(sums)
+
+    queries = list(map(int,input().split()))
+    for querie in queries:
+        print(bisect_left(res,querie) + 1)
+
+    
+
+def main():
+    # t = int(input())
+    t = 1
+    for _ in range(t):
+        solve()
+
+if __name__ == "__main__":
+    main()
