ad8c36de4682d12c715d438fd029cfd21b5a38a9
Estifanos Samson <143897248+thebaker-1@users.noreply.github.com>
2025-03-11 23:29:58 +0300

    Created 'A - A+ Student' by A2SV Hub

---
+++ A2SV G6 - Round #4 11-Mar-2025/A - A+ Student 303387.py
@@ -0,0 +1,31 @@
+# Problem: A - A+ Student - https://codeforces.com/gym/590053/problem/A
+
+import sys
+input = sys.stdin.readline
+
+def solve():
+    a,b,c= map(int, input().split())
+    res = []
+    if max(b,c) >= a:
+        res.append(max(b,c) - a + 1)
+    else:
+        res.append(0)
+    if max(a,c) >= b:
+        res.append(max(a,c) - b + 1)
+    else:
+        res.append(0)
+    if max(a,b) >= c:
+        res.append(max(a,b) - c+1)
+    else:
+        res.append(0)
+    print(*res)
+
+
+def main():
+    t = int(input())
+    #t = 1
+    for _ in range(t):
+        solve()
+
+if __name__ == "__main__":
+    main()
