1f0badc3d682705bd8603ee7443bf7bdd2b98137
Estifanos Samson <143897248+thebaker-1@users.noreply.github.com>
2025-03-11 23:28:25 +0300

    Created 'D - Meklit's Chat Screen' by A2SV Hub

---
+++ A2SV G6 - Round #6 11-Mar-2025/D - Meklit's Chat Screen 303382.py
@@ -0,0 +1,30 @@
+# Problem: D - Meklit's Chat Screen - https://codeforces.com/gym/594077/problem/D
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def solve():
+    n, m = map(int, input().split())
+    nums = list(map(int, input().split()))
+    q = deque()
+    unique = set()
+    for i in nums:
+        if i not in unique:
+            if len(q) == m:
+                x = q.pop()
+                unique.discard(x)
+            q.appendleft(i)
+            unique.add(i)
+    print(len(q))
+    print(*q)
+
+    
+
+def main():
+    t = 1
+    for _ in range(t):
+        solve()
+
+if __name__ == "__main__":
+    main()
