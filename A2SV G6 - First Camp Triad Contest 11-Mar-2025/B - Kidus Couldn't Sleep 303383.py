431747fa15ca9cd64b889d86ebb616e3f96cb89a
Estifanos Samson <143897248+thebaker-1@users.noreply.github.com>
2025-03-11 23:28:50 +0300

    Created 'B - Kidus Couldn't Sleep' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 11-Mar-2025/B - Kidus Couldn't Sleep 303383.py
@@ -0,0 +1,26 @@
+# Problem: B - Kidus Couldn't Sleep - https://codeforces.com/gym/589822/problem/B
+
+import sys
+input = sys.stdin.readline
+
+def solve():
+    n, m = map(int, input().split())
+    nums = list(map(int, input().split()))
+    l = 0
+    sum_sep = sum(nums[:m])
+    sum_t = sum_sep
+    for r in range(m,len(nums)):
+        sum_sep += nums[r] - nums[l]
+        sum_t += sum_sep
+        l += 1
+    print(sum_t / (n-m+1))
+
+
+def main():
+    # t = int(input())
+    t = 1
+    for _ in range(t):
+        solve()
+
+if __name__ == "__main__":
+    main()
