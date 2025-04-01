bfb818ff549837eecb0d59300925bf8b8b409dee
Estifanos Samson <143897248+thebaker-1@users.noreply.github.com>
2025-04-01 16:29:41 +0300

    Created 'E - Equalizing Arrays' by A2SV Hub

---
+++ A2SV G6 - Round #3 01-Apr-2025/E - Equalizing Arrays 323215.py
@@ -0,0 +1,31 @@
+# Problem: E - Equalizing Arrays - https://codeforces.com/gym/588468/problem/E
+
+from itertools import accumulate
+import sys
+# input = sys.stdin.readline
+
+def solve():
+    n = int(input())
+    # Read two integers separated by space
+    # n, m = map(int, input().split())
+    # Read a list of integers separated by space
+    nums = list(map(int, input().split()))
+    m = int(input())
+    nums2 = list(map(int, input().split()))
+    a= list(accumulate(nums))
+    b = list(accumulate(nums2))
+    res = (len(set(a) & set(b)))
+    if res and a[-1] == b[-1]:
+        print(res)
+    else:
+        print(-1)
+
+
+def main():
+    # t = int(input())
+    t = 1
+    for _ in range(t):
+        solve()
+
+if __name__ == "__main__":
+    main()
